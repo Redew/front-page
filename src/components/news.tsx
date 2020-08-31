89232f9f52b5ce0a9e7bf24f757577acae64dce8
Ford <c.b.simpson.j@gmail.com>
2020-08-31 12:28:14 -0600

    Update news.tsx

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -8,10 +8,12 @@ export default () => {
 			<div className={style.newsCont}>
 				<h1><i class="fas fa-newspaper"></i> Latest News</h1>
 				<div className={style.newsGrid}> 
-					<NewsFetcher />
+					<div>
+						<NewsFetcher />
+					</div>
 				
-					<div className={style.shadow}>
-						<a class="twitter-timeline" data-width="600" data-height="875" data-chrome="nofooter noborders" href="https://twitter.com/SmogonU?ref_src=twsrc%5Etfw">Tweets by SmogonU</a>
+					<div className={[style.shadow, style.twitter].join(" ")}>
+						<a class="twitter-timeline" data-height="900" data-chrome="nofooter noborders" href="https://twitter.com/SmogonU?ref_src=twsrc%5Etfw">Tweets by SmogonU</a>
 					</div>
 				</div>
 			</div>
